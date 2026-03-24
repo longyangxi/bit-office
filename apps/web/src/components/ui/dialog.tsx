3f93ebeab6f90fe5d3b245261353f7f7bf7750d8
marklong <mark.long@holinova.com>
2026-03-24 13:25:13 -0700

    Nova: style: thicken modal border from 1px #302a38 to 1.5 [agent-he0Yfw]

--- apps/web/src/components/ui/dialog.tsx
+++ apps/web/src/components/ui/dialog.tsx
@@ -45,7 +45,7 @@ const DialogContent = React.forwardRef<
         className={cn(
           "fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2",
           "w-[90%] max-w-[520px] max-h-[90vh]",
-          "bg-term-panel border border-border shadow-xl",
+          "bg-term-panel border-[1.5px] border-term-green-dim shadow-xl",
           "flex flex-col overflow-hidden",
           "font-mono text-term text-foreground",
           "data-[state=open]:animate-in data-[state=open]:fade-in-0 data-[state=open]:zoom-in-[0.97] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%]",
